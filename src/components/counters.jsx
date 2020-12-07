76f84f50501640e5de63b7d7393b7f48b813ce45
Nathan Rice <nrice778@gmail.com>
2020-12-07 16:55:08 -0600

    Added navbar, lifted up state, destructured Args

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,19 +2,25 @@ import React, { Component } from 'react';
 import Counter from './counter';
 
 class Counters extends Component {
-    state = { 
-        counters: [
-            { id: 1, value: 0 },
-            { id: 2, value: 0 },
-            { id: 3, value: 0 },
-            { id: 4, value: 0 },
-            { id: 5, value: 0 },
-        ]
-     }
-    render() { 
+    render() {
+        const { onReset, counters, onDelete, onIncrement } = this.props;
+
         return ( 
             <div>
-                { this.state.counters.map(counter => <Counter key={counter.id} />)}
+                <button
+                    onClick={onReset}
+                    className="btn btn-primary btm-sm m-2"
+                >
+                    Reset
+                </button>
+                { counters.map(counter => (
+                    <Counter 
+                        key={counter.id} 
+                        onDelete={onDelete}
+                        onIncrement={onIncrement}
+                        counter={counter}
+                    />
+                ))}
             </div> 
         );
     }
