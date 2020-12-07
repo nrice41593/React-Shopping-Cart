afffd3b2dfb26449f4bdfdf21f6784cf4fb226e3
Nathan Rice <nathan@Nathans-MacBook-Pro.local>
2020-12-07 13:24:16 -0600

    transfer project to windows os

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,16 +6,28 @@ export default class Counter extends Component {
     state = {
         count: 0
     };
-    render() { 
-        let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
 
+    handleIncrement = product => {
+        this.setState({count: this.state.count + 1})
+    }
+
+    render() { 
         return (
             <div>
-                <span className={ classes }>{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
+                <span className={ this.getBadgeClasses() }>{this.formatCount()}</span>
+                <button 
+                onClick={() => this.handleIncrement(product)} 
+                className="btn btn-secondary btn-sm">
+                    +
+                </button>
             </div>
         );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
     }
 
     formatCount(){
